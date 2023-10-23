546639da7a7356efe9d8fd852e17ac34fc01e447
FacuCasado <facufcasado@gmail.com>
2023-10-23 19:24:12 -0300

    routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,26 @@
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, useLocation } from 'react-router-dom';
 import Login from './views/app-views/Login';
+import Home from './views/app-views/Home';
+import RequireLogin from './auth/requireLogin';
+import Nav from './components/Nav';
 
 function App() {
+	const location = useLocation();
 	return (
 		<div>
+			{location.pathname !== '/' && <Nav />}
 			<Routes>
 				<Route
 					path="/"
 					element={<Login />}
+				/>
+				<Route
+					path="/home"
+					element={
+						<RequireLogin>
+							<Home />
+						</RequireLogin>
+					}
 				/>
 			</Routes>
 		</div>
