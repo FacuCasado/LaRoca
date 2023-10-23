dbe32e6e169428aac4ef7b665c67282c00c05ffd
FacuCasado <facufcasado@gmail.com>
2023-10-23 19:24:50 -0300

    redux

--- client/src/Redux/reducer.js
+++ client/src/Redux/reducer.js
@@ -1,8 +1,18 @@
+import { USER_SIGN_IN } from './userActions';
+
 const initialState = {
 	isLogin: false,
 	userData: {},
 };
 
-function rootReducer(state = initialState, action) {}
+function rootReducer(state = initialState, action) {
+	switch (action.type) {
+		case USER_SIGN_IN:
+			return { ...state, isLogin: true, userData: action.payload };
+
+		default:
+			return state;
+	}
+}
 
 export default rootReducer;
